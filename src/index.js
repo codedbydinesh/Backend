887e6b4df8b620ce71db1265b71ddac7118126bc
DineshKumarKori <dineshkumarkori978@gmail.com>
2025-07-23 17:19:36 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -14,7 +14,22 @@ const app = express();
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error)=>{
+        console.log('ERROR:', error);
+        throw error;
+        
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed!!!", err);
+    
+})
 
 
 
